531a414a2938d74197a0d4a5ff49f1a5b688772d
usuario <usuario@10.7.1.78>
2025-02-03 11:19:45 +0100

    Inicial. Primer commit.

---
+++ app-demo/src/main/java/prsw/app/demo/AppDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prsw.app.demo;
+
+/**
+ *
+ * @author usuario
+ */
+public class AppDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
